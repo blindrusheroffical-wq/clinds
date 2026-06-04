7d7d5199b4552bc02b56df668adf89249ef2e8d1
blindrusheroffical-wq <blindrusheroffical@gmail.com>
2026-06-04 10:56:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClientPOS from '../ClientPOS'
+import ClientPOS from './ClientPOS'
+
+function App() {
+  return (
+    <div style={{ margin: 0, padding: 0, minHeight: '100vh', backgroundColor: '#f4f7f6' }}>
+      <ClientPOS />
+    </div>
+  )
+}
+
+export default App
